31d94c28785bd76cd1602e74cfc6bcc082be8ddc
Josh Shone <josh.shone@gmail.com>
2020-05-27 12:24:38 +0200

    Refactor Graph()
    - Move styles into .tsx with Material-UI flavored CSSinJS
    - Eliminate useRef's for view ranges
    - Only perform requestAnimationFrame loop when necessary
    - Refactor SVG viewBox Y-axis range to be [0, max-value] instead of [0, 1] which makes the path generation cleaner
    - Make constants for some magic numbers
    - Re-use getViewBox() to avoid partially re-implementing it
    - General code cleanup and reformatting
    - Additional comments and TODOs

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,42 +1,102 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-import './Graph.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+const DEFAULT_GRAPH_RANGE_MS = 60 * 1000;
+const POLLING_INTERVAL_MS = 3000;
+
+const useStyles = makeStyles({
+  root: {
+    minHeight: '15rem',
+    cursor: 'grab',
+    flexGrow: 1,
+  },
+  path: {
+    fill: 'none',
+    strokeWidth: POLLING_INTERVAL_MS * 0.75,
+    transform: 'scaleY(-1) translateY(-100%)', // Flip vertically so a Y value of zero appears on the bottom
+  }
+});
+
+// TODO: source default color from theme palette
 
 export default function Graph( {data, keys=['value'], colors=['#839496']} ) {
+
+  const classes = useStyles();
+
   const firstTimestamp = data[0].timestamp;
 
-  function now() {
-    return (new Date()).getTime();
-  }
-
-  const [rangeDuration, setRangeDuration] = useState(now() - firstTimestamp);
-  const rangeDurationRef = useRef();
-  rangeDurationRef.current = rangeDuration;
-
+  // View ranges
+  const [rangeDuration, setRangeDuration] = useState(DEFAULT_GRAPH_RANGE_MS);
   const [rangeUntil, setRangeUntil] = useState('now');
-  const rangeUntilRef = useRef();
-  rangeUntilRef.current = rangeUntil;
 
-  const svgRef = useRef(null);
-
-  // Map data to SVG paths
+  // Add up values in each entry to get stack segment positions
   const stacks = data.map(entry => {
     let y = 0;
     return keys.map(key => [y, y += (entry[key] || 0)]);
   });
-  const maxValue = Math.max(...stacks.map(stack => stack[keys.length-1][1]));
+
+  // Map stacks to SVG paths
+  // Each key is given a separate path so it can be assigned its own color
   const paths = keys.map((key, keyIndex) => (
     <path
+      className={classes.path}
       key={key}
       stroke={colors[keyIndex]}
-      d={'M0,0 ' + stacks.map((stack, index) => `M${data[index].timestamp - firstTimestamp},${stack[keyIndex][0] / maxValue} V${stack[keyIndex][1] / maxValue} `).join('')}
+      d={
+        stacks.map((stack, index) =>
+          `M${data[index].timestamp - firstTimestamp},${stack[keyIndex][0]} V${stack[keyIndex][1]} `
+        ).join('')
+      }
     />
   ));
-
+  // TODO: Splitting up paths based on a certain duration threshold may improve React reconciliation performance
   // TODO: add X-axis ticks
 
+  // Get the highest point on the graph, to determine the viewBox height
+  const maxValue = Math.max(...stacks.map(stack => stack[keys.length-1][1]));
+
+  function getViewBox() {
+    const currentRangeUntil = rangeUntil === 'now' ? now() : rangeUntil;
+
+    const minX = (currentRangeUntil - rangeDuration) - firstTimestamp;
+    const minY = 0;
+    const width = rangeDuration;
+    const height = maxValue;
+
+    return `${minX} ${minY} ${width} ${height}`;
+  }
+
+  function zoom(factor: number) {
+    const currentRangeUntil = rangeUntil === 'now' ? now() : rangeUntil;
+    let newRangeUntil = currentRangeUntil + (((rangeDuration * factor) - rangeDuration) / 2);
+    newRangeUntil = Math.min(newRangeUntil, now());
+    setRangeUntil(newRangeUntil >= now() ? 'now' : newRangeUntil);
+
+    let newRangeDuration = rangeDuration * factor;
+    newRangeDuration = Math.min(newRangeDuration, newRangeUntil - firstTimestamp);
+    setRangeDuration(newRangeDuration);
+  }
+
+  const svgRef = useRef(null);
+
+  useEffect(() => {
+    // If the range is locked to 'now', smoothly scroll the graph forward in time
+    // by animating the viewBox
+    if (rangeUntil === 'now') {
+      let animationFrameId = requestAnimationFrame(function callback(timestamp) {
+        if (svgRef.current) {
+          // A Ref is used to directly set the DOM attribute to avoid thrashing React rendering
+          svgRef.current.setAttribute('viewBox', getViewBox());
+        }
+        animationFrameId = requestAnimationFrame(callback);
+      });
+      return () => cancelAnimationFrame(animationFrameId);
+    }
+  }, [rangeUntil, rangeDuration, firstTimestamp]);
+
   function onWheel(event) {
-    zoom(event.deltaY < 0 ? 0.1 : -0.1);
+    zoom(event.deltaY > 0 ? 1.1 : 1/1.1);
   }
 
   function onMouseDown(event) {
@@ -48,7 +108,9 @@ export default function Graph( {data, keys=['value'], colors=['#839496']} ) {
       const deltaXRatio = (event.pageX - lastPageX) / boundingRect.width;
       draggedRangeUntil -= rangeDuration * deltaXRatio;
       draggedRangeUntil = Math.max(draggedRangeUntil, firstTimestamp + rangeDuration);
-      setRangeUntil(draggedRangeUntil >= now() ? 'now' : draggedRangeUntil); // TODO: throttle?
+      setRangeUntil(draggedRangeUntil >= now() ? 'now' : draggedRangeUntil);
+      // TODO: calling setRangeUntil() here causes a re-render (and rebuild of the graph) on every
+      // mouse move event, which is quite inefficient. Need to find a way to update the viewBox cheaply..
       lastPageX = event.pageX;
     }
     window.addEventListener('mousemove', onMousemove);
@@ -57,40 +119,20 @@ export default function Graph( {data, keys=['value'], colors=['#839496']} ) {
     }, {once: true});
   }
 
-  function zoom(amount: number) {
-    const currentRangeUntil = rangeUntil === 'now' ? now() : rangeUntil;
-    let newRangeUntil = currentRangeUntil - (rangeDuration * (amount / 2));
-    newRangeUntil = Math.min(newRangeUntil, now());
-    setRangeUntil(newRangeUntil >= now() ? 'now' : newRangeUntil);
-    rangeUntilRef.current = newRangeUntil;
-
-    let newRangeDuration = rangeDuration * (1 - amount);
-    newRangeDuration = Math.min(newRangeDuration, newRangeUntil - firstTimestamp);
-    setRangeDuration(newRangeDuration);
-  }
-
-  function getViewBox() {
-    const currentRangeUntil = rangeUntil === 'now' ? now() : rangeUntil;
-    // TODO: allow SVG origin to start at timestamp 0 instead of firstTimestamp
-    return `${(currentRangeUntil - rangeDuration) - firstTimestamp} 0 ${rangeDuration} 1`;
-  }
-
-  useEffect(() => {
-    let animationFrameId = requestAnimationFrame(function callback(timestamp) {
-      if (svgRef.current) {
-        const currentRangeUntil = rangeUntilRef.current === 'now' ? now() : rangeUntilRef.current;
-        svgRef.current.viewBox.baseVal.x = (currentRangeUntil - rangeDurationRef.current) - firstTimestamp;
-      }
-      animationFrameId = requestAnimationFrame(callback);
-    });
-    return () => cancelAnimationFrame(animationFrameId);
-  }, [firstTimestamp]);
-
   return (
-    <svg className="graph" viewBox={getViewBox()} preserveAspectRatio="none" ref={svgRef} onWheel={onWheel} onMouseDown={onMouseDown}>
-      <g>
-        {paths}
-      </g>
+    <svg
+      className={classes.root}
+      viewBox={getViewBox()}
+      preserveAspectRatio="none"
+      ref={svgRef}
+      onWheel={onWheel}
+      onMouseDown={onMouseDown}
+    >
+      {paths}
     </svg>
   )
+}
+
+function now() {
+  return (new Date()).getTime();
 }
