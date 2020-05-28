eab86723a70027741afa4203d8c60130c72b8c51
Josh Shone <josh.shone@gmail.com>
2020-05-28 22:23:25 +0200

    Limit server data entries to 200 and add port and generate_random CLI options

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,17 @@
+const {argv} = require('yargs'); // TODO: declare args with description and types
+
 const express = require('express');
 const path = require('path');
 const app = express();
 
 const {google} = require('googleapis');
 const analytics = google.analytics('v3');
-const googleServiceAccountAuth = require('../google-service-account-auth.json');
 
 import {Metrics} from '../types/metrics';
 
+const DEFAULT_PORT = 8080;
 const DATA_FETCH_INTERVAL_MS = 3000;
+const MAX_DATA_ENTRIES = 200;
 
 const data: Metrics[] = [];
 
@@ -17,43 +20,9 @@ interface DataListener {
 }
 const dataListeners: DataListener[] = [];
 
-(async function main() {
-
-  const jwtClient = new google.auth.JWT({
-    email: googleServiceAccountAuth.client_email,
-    key: googleServiceAccountAuth.private_key,
-    scopes: ['https://www.googleapis.com/auth/analytics.readonly'],
-  });
-
-  await new Promise((resolve, reject) => jwtClient.authorize((error, tokens) => {
-    if (error) reject(error);
-    else resolve();
-  }));
-  // TODO: don't start Express if JWT authorization fails
-
-  while (true) {
-    // Note: I've seen similar projects that use setInterval() for this polling loop, but
-    // it can cause a storm of parallel API requests because setInterval doesn't wait for
-    // previous iterations to complete in the case of slow network conditions.
-
-    const newData = await fetchFromGoogleAnalytics(jwtClient);
-    // TODO: catch API request failures and log
-
-    data.push(newData); // TODO: limit number of entries
-
-    // Notify clients listening with server-sent events
-    while (dataListeners.length > 0) {
-      const callback = dataListeners.pop();
-      callback(newData);
-    }
-
-    // TODO: account for duration of Analytics API fetch
-    await new Promise(resolve => setTimeout(resolve, DATA_FETCH_INTERVAL_MS));
-
-    // TODO: explicitly listen for exit signal?
-  }
-})();
-
+// For development convenience this server currently both acts as the API and
+// static fileserver for the client-side assets.
+// In an actual production deployment use of a separate CDN for the static assets would be more appropriate.
 app.use(express.static(path.join(__dirname, '../build')));
 
 function sendIndexHtml(request, response) {
@@ -65,6 +34,7 @@ app.get('/browsers', sendIndexHtml);
 app.get('/os', sendIndexHtml);
 
 app.use(function(request, response, next) {
+  // Allow the create-react-app development server to connect
   response.header("Access-Control-Allow-Origin", "http://localhost:3000");
   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -85,10 +55,62 @@ app.get('/live-events', async (request, response) => {
   while (true) {
     const newData = await new Promise(resolve => dataListeners.push(resolve));
     response.write(`data: ${JSON.stringify(newData)}\n\n`);
+    // TODO: handle request close
   }
 });
 
-app.listen(8080, () => console.log(`Listening at http://localhost:8080`))
+if (argv.generate_random) {
+  randomGenerationUpdateLoop();
+} else {
+  googleAnalyticsUpdateLoop();
+}
+
+const port = argv.port || DEFAULT_PORT;
+
+app.listen(port, () => console.log(`Listening at http://:${port}`))
+
+async function googleAnalyticsUpdateLoop() {
+
+  const googleServiceAccount = require('../google-service-account.json');
+
+  const jwtClient = new google.auth.JWT({
+    email: googleServiceAccount.client_email,
+    key: googleServiceAccount.private_key,
+    scopes: ['https://www.googleapis.com/auth/analytics.readonly'],
+  });
+
+  await new Promise((resolve, reject) => jwtClient.authorize((error, tokens) => {
+    if (error) reject(error);
+    else resolve();
+  }));
+  // TODO: don't start Express if JWT authorization fails
+
+  while (true) {
+    // Note: I've seen similar projects that use setInterval() for this polling loop, but
+    // it can cause a storm of parallel API requests because setInterval doesn't wait for
+    // previous iterations to complete in the case of slow network conditions.
+
+    const newData = await fetchFromGoogleAnalytics(jwtClient);
+    // TODO: catch API request failures and log
+
+    data.push(newData);
+
+    if (data.length > MAX_DATA_ENTRIES) {
+      data.splice(0, data.length - MAX_DATA_ENTRIES);
+    }
+
+    // Notify clients listening with server-sent events
+    while (dataListeners.length > 0) {
+      const callback = dataListeners.pop();
+      callback(newData);
+    }
+
+    // TODO: account for duration of Analytics API fetch
+    await new Promise(resolve => setTimeout(resolve, DATA_FETCH_INTERVAL_MS));
+
+    // TODO: explicitly listen for exit signal?
+  }
+}
 
 async function fetchFromGoogleAnalytics(jwtClient): Promise<Metrics> {
 
@@ -127,14 +149,26 @@ async function fetchFromGoogleAnalytics(jwtClient): Promise<Metrics> {
   }
 }
 
-function generateInitialRandomData() {
-  const data = [];
+async function randomGenerationUpdateLoop() {
   const now = (new Date()).getTime();
-  const entries = 30;
-  for (let i=0; i < entries; i++) {
-    data.push(generateRandomData(now - ((entries - i) * DATA_FETCH_INTERVAL_MS)));
+
+  for (let i=0; i < MAX_DATA_ENTRIES; i++) {
+    data.push(generateRandomData(now - ((MAX_DATA_ENTRIES - i) * DATA_FETCH_INTERVAL_MS)));
   }
-  return data;
+
+  while (true) {
+    const newData = generateRandomData((new Date()).getTime());
+
+    data.push(newData);
+
+    // Notify clients listening with server-sent events
+    while (dataListeners.length > 0) {
+      const callback = dataListeners.pop();
+      callback(newData);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, DATA_FETCH_INTERVAL_MS));
+  }
 }
 
 function generateRandomData(timestamp): Metrics {
